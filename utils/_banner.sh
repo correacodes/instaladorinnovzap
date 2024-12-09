b2a65ce4ef578a9e1ac007e1cad0c73eeb84bc40
correacodes <m_costafilho@outlook.com>
2024-12-09 20:38:07 -0300

    Arquivos

---
+++ utils/_banner.sh
@@ -0,0 +1,39 @@
+print_banner() {
+  clear
+
+  printf "\n\n"
+
+  printf "${GREEN}";
+  printf "                                                     ▄▄█▀▀▀▀▀▀▀█▄▄  \n";
+  printf "                                                   ${GREEN}▄█▀${NC}   ${WHITE}▄▄${NC}      ${GREEN}▀█▄\n";
+  printf "                                                   ${GREEN}█${NC}    ${WHITE}███${NC}         ${GREEN}█\n";
+  printf "                                                   ${GREEN}█${NC}    ${WHITE}██▄         ${GREEN}█${NC}\n";
+  printf "                                                   ${GREEN}█${NC}     ${WHITE}▀██▄${NC} ${WHITE}██${NC}    ${GREEN}█\n";
+  printf "                                                   ${GREEN}█${NC}       ${WHITE}▀███▀${NC}    ${GREEN}█\n";
+  printf "                                                   ${GREEN}▀█▄           ▄█▀\n";
+  printf "                                                    ▄█    ▄▄▄▄█▀▀  \n";
+  printf "                                                    █  ▄█▀        \n";
+  printf "                                                    ▀▀▀▀          \n";
+  printf "${NC}";
+
+  printf "\n"
+
+  printf "${GREEN}";
+  printf "██╗███╗░░██╗███╗░░██╗░█████╗░██╗░░░██╗████████╗███████╗███╗░░██╗░█████╗░\n";
+  printf "██║████╗░██║████╗░██║██╔══██╗██║░░░██║╚══██╔══╝██╔════╝████╗░██║██╔══██╗\n";
+  printf "██║██╔██╗██║██╔██╗██║██║░░██║██║░░░██║░░░██║░░░█████╗░░██╔██╗██║██║░░██║\n";
+  printf "██║██║╚████║██║╚████║██║░░██║██║░░░██║░░░██║░░░██╔══╝░░██║╚████║██║░░██║\n";
+  printf "██║██║░╚███║██║░╚███║╚█████╔╝╚██████╔╝░░░██║░░░███████╗██║░╚███║╚█████╔╝\n";
+  printf "╚═╝╚═╝░░╚══╝╚═╝░░╚══╝░╚════╝░░╚═════╝░░░░╚═╝░░░╚══════╝╚═╝░░╚══╝░╚════╝░\n";
+  printf "${NC}";
+
+  printf "\n"
+
+  printf "${GREEN}";
+  printf "SISTEMA DE MULTIATENDIMENTO INNOVTECNO SISTEMAS\n";
+  printf "SUPORTE: +5543-99047081\n";
+  printf "${NC}";
+
+  printf "\n"
+}
+
